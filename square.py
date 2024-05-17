b320ce11bc04fd8fe21b73c298900b08c612c1a3
ark948 <armankarimi948@gmail.com>
2024-05-17 14:27:52 +0330

    Added type checks

--- square.py
+++ square.py
@@ -1,5 +1,11 @@
 class Square:
     def __init__(self, length) -> None:
+        if type(length) not in [int, float]:
+            raise TypeError("Length must be an integer or a float.")
+        
+        if length < 0:
+            raise ValueError("Length must not be negative.")
+
         self.length = length
 
     def area(self):
--- test_square.py
+++ test_square.py
@@ -7,6 +7,17 @@ class TestSquare(unittest.TestCase):
         area = square.area()
         self.assertEqual(area, 100)
 
+    def test_length_with_wrong_type(self):
+        # tests if the typeerror is raised if length was not int or float (wrong type)
+        with self.assertRaises(TypeError):
+            square = Square('10')
+
+    def test_length_with_zero_or_negative(self):
+        # tests if the valueerror is raised if length is negative
+        with self.assertRaises(ValueError):
+            square = Square(0)
+            square = Square(-1)
+
 
 # you can also delete this main function
 # and call python -m unittest -v (will find test modules)
